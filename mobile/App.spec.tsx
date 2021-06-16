0356cdc2df7a415d27a2959c1ad643151745a723
easy4docker <easy4docker@gmail.com>
2021-06-16 16:44:53 -0700

    update

---
+++ mobile/App.spec.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import App from "./App"
+
+describe("<App />", () => {
+  it("has 2 child", () => {
+    const tree: any = renderer.create(<App />).toJSON()
+    expect(tree?.children?.length).toBe(2)
+  })
+
+  xit("matches last snapshot", async () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  
+})
---
+++ mobile/src/index.spec.tsx
@@ -0,0 +1,5 @@
+describe("index", () => {
+    it("test case", () => {
+      expect(true).toBe(true)
+    })
+  })
